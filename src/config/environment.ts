5bc009527dcee74506e0b02ef7c6851023de6b2e
IgnacioNMiranda <i.miranda.f99@gmail.com>
2022-04-30 16:30:31 -0400

    fix: env vars names

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -5,7 +5,7 @@ export const environment = {
 
     namePropertyKey: process.env.NOTION_NAME_PROPERTY_KEY ?? 'Name',
     agePropertyKey: process.env.NOTION_AGE_PROPERTY_KEY ?? 'Age',
-    dayOfBirthPropertyKey: process.env.NOTION_DAY_OF_BIRTHDAY_PROPERTY_KEY ?? 'Day of birth',
+    dayOfBirthPropertyKey: process.env.NOTION_DAY_OF_BIRTH_PROPERTY_KEY ?? 'Day of birth',
   },
   twilio: {
     accountSId: process.env.TWILIO_ACCOUNT_SID,
